edc61f53022de5f097e4588cfc649d62fedc61c0
Влад Коваленко <vlad.kovalenko90092@gmailcom>
2026-07-05 17:45:05 +0300

    исправление: удалили неиспользуемую функцию sayHi, чтобы пройти проверку eslint

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-console.log('Hello from js-hello-world!');
 import _ from 'lodash';
+
+console.log('Hello from js-hello-world!');
 console.log(_.last(['one', 'two']));
+const badVar = 10;
+console.log(badVar);
